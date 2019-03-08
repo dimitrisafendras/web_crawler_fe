ce5f9803d127b3d9ccb1d34adf330e92e8fdb186
dimitrisafendras <dimitris.afendras@camelotglobal.com>
2019-03-08 11:22:52 +0200

    added axios for rest calls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
-import './App.css';
-import {request} from './api';
+import axios from 'axios';
+
+import './App.scss';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-          </header>
-          <main>
-          <button onClick={() => request('https://api.openbrewerydb.org/breweries')}>
-            request
-          </button>
-          </main>
-          <footer/>
-        </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [],
+        }
+    }
+
+    handleClick() {
+        axios.get('https://api.openbrewerydb.org/breweries')
+            .then(response => this.setState({data: response.data}))
+    }
+
+    render() {
+        const data = this.state.data;
+        return (
+            <div className="App">
+                <header className="App-header">
+                </header>
+                <main>
+                    <button onClick={() => this.handleClick()}>
+                        request
+                    </button>
+                    {data.map(i =>
+                        <div key={i.id}>
+                            {`${i.name}`}
+                        </div>
+                    )}
+                </main>
+                <footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
+
