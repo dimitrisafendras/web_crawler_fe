5ee94cb2e833b3e7467a7afa3a63a0a28d7d91da
dimitrisafendras <dimitris.afendras@camelotglobal.com>
2019-03-19 16:09:47 +0200

    refactored page logic and structure to match atomic design

--- src/App.js
+++ src/App.js
@@ -1,67 +1,13 @@
 import React, { Component } from 'react';
-import {fetch} from './api';
 import './App.scss';
 
-import {PrimaryBtnWithCell} from './molecules';
-import {
-    Header,
-    Footer, HomeMain,
-} from './organisms';
-import {HomeLayout} from './templates';
+import {HomePage} from './pages';
 
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            data: [],
-        }
-    }
-
-    onFetch() {
-        fetch()
-            .then(
-                response => { console.log('>>>', response.data)
-                    this.setState(
-                        {data: response.data}
-                    )}
-            )
-    }
-
-    onDelete() {
-        this.setState({data: []})
-    }
-
     render() {
-        const data = this.state.data;
         return (
             <div className="App">
-                <HomeLayout>
-                    <Header
-                        id={'header'}
-                        area={'header'}
-                    />
-                    <PrimaryBtnWithCell
-                        id={'fetch'}
-                        area={'fetch'}
-                        onClick={()=> this.onFetch()}
-                        text={'Fetch'}
-                    />
-                    <HomeMain
-                        id={'main'}
-                        area={'main'}
-                        data={data}
-                    />
-                    <PrimaryBtnWithCell
-                        id={'delete'}
-                        area={'delete'}
-                        onClick={()=> this.onDelete()}
-                        text={'Delete'}
-                    />
-                    <Footer
-                        id={'footer'}
-                        area={'footer'}
-                    />
-                </HomeLayout>
+                <HomePage id={'homePage'}/>
             </div>
         );
     }
---
+++ src/pages/homePage/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './HomePage.scss';
+
+import {
+    PrimaryBtnWithCell
+} from '../../molecules/primaryBtnWithCell';
+import {
+    Header,
+    Footer,
+    HomeMain,
+} from '../../organisms';
+import {
+    HomeLayout
+} from '../../templates';
+import {fetch} from '../../api';
+
+export class HomePage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [],
+        }
+    }
+
+    onFetch() {
+        fetch()
+            .then(
+                response => { console.log('>>>', response.data)
+                    this.setState(
+                        {data: response.data}
+                    )}
+            )
+    }
+
+    onDelete() {
+        this.setState({data: []})
+    }
+
+     render() {
+         const data = this.state.data;
+         return (
+              <HomeLayout>
+                  <Header
+                      id={'header'}
+                      area={'header'}
+                  />
+                  <PrimaryBtnWithCell
+                      id={'fetch'}
+                      area={'fetch'}
+                      onClick={()=> this.onFetch()}
+                      text={'Fetch'}
+                  />
+                  <HomeMain
+                      id={'main'}
+                      area={'main'}
+                      data={data}
+                  />
+                  <PrimaryBtnWithCell
+                      id={'delete'}
+                      area={'delete'}
+                      onClick={()=> this.onDelete()}
+                      text={'Delete'}
+                  />
+                  <Footer
+                      id={'footer'}
+                      area={'footer'}
+                  />
+              </HomeLayout>
+          );
+     }
+}
